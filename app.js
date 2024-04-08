fe9699c527e15887119db3e2887c20a22d5edd89
ansaralyh <ansaralyh@gmail.com>
2024-04-08 16:01:52 +0500

    create user done

--- app.js
+++ app.js
@@ -1,6 +1,12 @@
 const express = require('express');
 const app = express();
+const cors = require('cors');
 
+const bodyParser = require('body-parser');
+app.use(express.json());
+app.use(bodyParser.json());
+
+app.use(cors("*"));
 const adminRoutes = require('./routes/index.routes')
 
 app.use('/api/v1',adminRoutes);
--- coontrollers/userController.js
+++ coontrollers/userController.js
@@ -1,16 +1,36 @@
 const User = require('../models/userModel');
-const catchAsyncErrors = require('../middlewares/catchAsyncErrors')
-
-var bcrypt = require("bcryptjs");
-var salt = bcrypt.genSaltSync(10);
-
+const catchAsyncErrors = require('../middlewares/catchAsyncErrors');
+const bcrypt = require("bcryptjs");
 
 exports.store = catchAsyncErrors(async (req, res, next) => {
-    const hashedPassword = await bcrypt.hash(req.body.password, salt);
-    req.body.password = hashedPassword;
-    const result = await users.create(req.body);
-    res.status(200).json({
-      success: true,
-      messege: "User created successfully",
-    });
-  });
+    const { name, email, password } = req.body;
+
+    if (!name || !email || !password) {
+        return res.status(400).json({
+            success: false,
+            message: "Fields missing"
+        });
+    }
+
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    try {
+        const user = await User.create({
+            name,
+            email,
+            password: hashedPassword
+        });
+
+        res.status(200).json({
+            success: true,
+            message: "User created successfully",
+            user
+        });
+    } catch (error) {
+        return res.status(500).json({
+            success: false,
+            message: "User creation failed",
+            error: error.message
+        });
+    }
+});
