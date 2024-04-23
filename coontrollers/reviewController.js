3a40745a1f7efb64480264662c98e0bdcd876ec3
Zultaif Hassan <zultaifhassan110@gmail.com>
2024-04-23 20:57:39 +0500

    new

--- coontrollers/reviewController.js
+++ coontrollers/reviewController.js
@@ -1,4 +1,5 @@
 const Review = require('../models/reviewModel')
+const catchAsyncErrors = require('../middlewares/catchAsyncErrors');
 
 exports.createReview = async (req, res) => {
     try {
@@ -11,6 +12,35 @@ exports.createReview = async (req, res) => {
         res.status(500).json({message: error.message})
     }
 }
+
+
+//Get all products
+exports.getAllReviews = catchAsyncErrors(async(req,res,next) =>{
+  const reviews = await Review.find();
+  if(!reviews){
+      return next(new ErrorHandler("Products not found"))
+  }
+  res.status(200).json({
+      success:true,
+      result:reviews
+  })
+})
+
+
+
+// get single review
+
+exports.getSingleReview = catchAsyncErrors(async(req,res,next) =>{
+  const Review = req.params.id;
+  const requiredReview = await Review.findById(req.params.id);
+  if(!Review){
+      return next(new ErrorHandler("Product not found"))
+  }
+  res.status(200).json({
+      success:true,
+      result:requiredReview
+  })
+})
 
 
 // get total number of reviews
--- routes/review.routes.js
+++ routes/review.routes.js
@@ -1,11 +1,13 @@
 const express = require('express');
-const { createReview, getTotalReviews } = require('../coontrollers/reviewController');
+const { createReview, getTotalReviews, getAllReviews } = require('../coontrollers/reviewController');
 
 
 const router = express.Router();
 
 router.post('/', createReview);
+router.get('/', getAllReviews)
 router.get('/count', getTotalReviews)
+
 
 
 
